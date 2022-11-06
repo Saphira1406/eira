de769d9c54b9bd61c6ec149ac48d822eab46b806
Saphira1406 <mellone.flore@gmail.com>
2022-11-06 13:10:26 -0300

    agregado de diseño al componente FormEjercicios

--- eira/src/components/FormEjercicios.jsx
+++ eira/src/components/FormEjercicios.jsx
@@ -1,30 +1,28 @@
 import { useEffect, useState } from "react"
 import ObjectId from "bson-objectid"
-
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 function FormEjercicios(props) {
-
     const [ejercicio, setEjercicio] = useState("")
     const [video, setVideo] = useState("")
     const [ejercicios, setEjercicios] = useState([])
     const [repeticiones, setRepeticiones] = useState("")
 
     function agregarEjercicio(ev) {
-    
-       setEjercicios(prev => 
-        [
-            ...prev, 
-            {
-                id: ObjectId(),
-                ejercicio,
-                video,
-                repeticiones
-            }]);
-        
-       
-       setEjercicio("");
-       setVideo("");
-       setRepeticiones("");
+        setEjercicios(prev =>
+            [
+                ...prev,
+                {
+                    id: ObjectId(),
+                    ejercicio,
+                    video,
+                    repeticiones
+                }
+            ]);
+        setEjercicio("");
+        setVideo("");
+        setRepeticiones("");
     }
 
     useEffect(() => {
@@ -32,35 +30,36 @@ function FormEjercicios(props) {
     }, [ejercicios])
 
     return(
-        <>
-            <div className="mb-3">
-                    <label htmlFor="ejercicio" className="form-label">Nombre ejercicio</label>
-                    <input type="text" className="form-control" id="ejercicio" name="ejercicio" value={ejercicio} onChange={(ev) => setEjercicio(ev.target.value)} />
-                </div>
-
-                <div className="mb-3">
-                    <label htmlFor="repeticiones" className="form-label">Repeticiones</label>
-                    <input type="number" className="form-control" id="repeticiones" name="repeticiones" value={repeticiones} onChange={e => setRepeticiones(e.target.value)} />
-                </div>
-
-                <div className="mb-3">
-                    <label htmlFor="video" className="form-label">Video</label>
-                    <input type="text" className="form-control" id="video" name="video" value={video} onChange={e => setVideo(e.target.value)} />
-                </div>
-
-
-                <button type="button" className="btn btn-success" onClick={agregarEjercicio}>agregar</button>
-
-                <p>Lista de ejercicio agregadas</p>
+        <div className="mt-3">
+            <Form.Group className="mb-4" controlId="ejercicio">
+                <Form.Control type="text" placeholder="Nombre del ejercicio" name="ejercicio" value={ejercicio} onChange={(ev) => setEjercicio(ev.target.value)}/>
+            </Form.Group>
+            <Form.Group className="mb-4" controlId="repeticiones">
+                <Form.Control type="text" placeholder="Indicar cantidad de repeticiones" name="repeticiones" value={repeticiones} onChange={e => setRepeticiones(e.target.value)}/>
+            </Form.Group>
+            <Form.Group className="mb-4" controlId="video">
+                <Form.Control type="text" placeholder="URL de video" name="video" value={video} onChange={e => setVideo(e.target.value)}/>
+            </Form.Group>
+            <div className="d-flex justify-content-center">
+                <Button type="button" onClick={agregarEjercicio} className="btn btn-agregar">
+                    Agregar
+                </Button>
+            </div>
+            <p className="fw-bold text-center mt-4">Lista de ejercicios agregados:</p>
+            <ul className="lista-agregada-meds">
                 {ejercicios.map((ejercicio, i) =>
-                    <ul key={i}>
-                      
-                        <li>{ejercicio.ejercicio}</li>
-                        <li>{ejercicio.repeticiones}</li>
-                        <li><a href={ejercicio.video} target="_blank" rel="noreferrer">ver video</a></li>
-                    </ul>
+                <li key={i} className="shadow mb-3">
+                    <span className="fw-bold">{ejercicio.ejercicio}</span><br/>
+                    <span className="me-5">
+                        <span className="fw-bold">Cantidad de repeticiones:</span> {ejercicio.repeticiones}
+                    </span>
+                    <span className="me-5">
+                        <span className="fw-bold">Video:</span> {ejercicio.video}
+                    </span>
+                </li>
                 )}
-        </>
+            </ul>
+        </div>
     )
 }
 
