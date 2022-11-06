dfd99fdbb76eba46a3a7c9637553f70b72c09051
Saphira1406 <mellone.flore@gmail.com>
2022-11-06 13:02:31 -0300

    agregado de diseño al componente FormMedicamentos

--- eira/src/components/FormComida.jsx
+++ eira/src/components/FormComida.jsx
@@ -4,16 +4,12 @@ import Button from 'react-bootstrap/Button'
 
 
 function FormComida(props) {
-
     const [comida, setComida] = useState("")
     const [comidas, setComidas] = useState([])
 
-
     function agregarComida(ev) {
         console.log("Agregar comi", comida);
-
         setComidas(prev => [...prev, comida]);
-
         setComida("");
     }
 
@@ -22,25 +18,25 @@ function FormComida(props) {
     }, [comidas])
 
     return (
-        <>
-        <Form.Group className="my-3" controlId="comida">
-            <Form.Control type="text" placeholder="Nombre comida a restringir" name="comida" value={comida} onChange={(ev) => setComida(ev.target.value)}/>
-        </Form.Group>
-        <div className="d-flex justify-content-center">
-            <Button type="button" onClick={agregarComida} className="btn btn-agregar">
-                Agregar
-            </Button>
-        </div>
+        <div>
+            <Form.Group className="my-3" controlId="comida">
+                <Form.Control type="text" placeholder="Nombre comida a restringir" name="comida" value={comida} onChange={(ev) => setComida(ev.target.value)}/>
+            </Form.Group>
+            <div className="d-flex justify-content-center">
+                <Button type="button" onClick={agregarComida} className="btn btn-agregar">
+                    Agregar
+                </Button>
+            </div>
 
-        <p className="fw-bold text-center mt-4">Lista de comidas restringidas:</p>
-        <ul className="lista-agregada d-flex justify-content-center">
-            {comidas.map((comida, i) =>
-            <li key={i} className="shadow mx-2">
-                {comida}
-            </li>
-            )}
-        </ul>
-        </>
+            <p className="fw-bold text-center mt-4">Lista de comidas restringidas:</p>
+            <ul className="lista-agregada d-flex justify-content-center">
+                {comidas.map((comida, i) =>
+                <li key={i} className="shadow mx-2">
+                    {comida}
+                </li>
+                )}
+            </ul>
+        </div>
     )
 }
 
--- eira/src/components/FormMedicamentos.jsx
+++ eira/src/components/FormMedicamentos.jsx
@@ -1,18 +1,17 @@
 import { useEffect, useState } from "react"
 import ObjectId from "bson-objectid"
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 
 function FormMedicamentos(props) {
-    
     const [medicamento, setMedicamento] = useState("")
     const [medicamentos, setMedicamentos] = useState([])
     const [horas, setHoras] = useState("")
     const [fecha, setFecha] = useState("")
 
-
     function agregarMedicamento() {
         console.log("agrego a lista de medicamentos")
-       
         setMedicamentos( prev =>
                 [
                     ...prev,
@@ -24,10 +23,8 @@ function FormMedicamentos(props) {
                     }
                 ]
         )
-       
         setMedicamento("")
         setHoras("")
-        
     }
 
     useEffect(() => {
@@ -35,40 +32,38 @@ function FormMedicamentos(props) {
     }, [medicamentos])
 
     return(
-        <div>
-                    <div className="mb-3">
-                        <label htmlFor="medicamento" className="form-label">Nombre medicamento</label>
-                        <input type="text" className="form-control" id="medicamento" name="medicamento" value={medicamento} onChange={(ev) => setMedicamento(ev.target.value)}/>
-                    </div>
-
-                    <div className="mb-3">
-                        <label htmlFor="horas" className="form-label">Frecuencia horaria</label>
-                        <input type="number" className="form-control" id="horas" name="horas" value={horas} onChange={(ev) => setHoras(ev.target.value)} />
-                    </div>
-
-                    <div className="mb-3">
-                        <label htmlFor="finalizacion" className="form-label">¿Fecha finalizacion?</label>
-                        <input type="date" className="form-control" id="finalizacion" name="finalizacion" value={fecha} onChange={ (ev) => setFecha(ev.target.value) } />
-                    </div>
-
-                    <button type="button" className="btn btn-success mt-3" onClick={agregarMedicamento}>agregar med</button>
-
-                    <p>Lista de medicamentos agregadas</p>
-                   
-                    <ul>
-                        {medicamentos.map((medicamento, i) => 
-                        
-                        <li key={i}>
-                            {medicamento.nombre}<br/>
-                            {medicamento.horas}
-                            {medicamento.fecha}
-                        </li>
-                        )}
-                
-                        
-                    </ul>
-                    --------------------------------------------
-        
+        <div className="mt-3">
+            <Form.Group className="mb-4" controlId="medicamento">
+                <Form.Control type="text" placeholder="Nombre del medicamento" name="medicamento" value={medicamento} onChange={(ev) => setMedicamento(ev.target.value)}/>
+            </Form.Group>
+            <Form.Group className="mb-4" controlId="horas">
+                <Form.Control type="number" placeholder="¿Cáda cuánto tiempo debe tomar el medicamento? (Indicar en cant. de horas)" name="horas" value={horas} onChange={(ev) => setHoras(ev.target.value)}/>
+            </Form.Group>
+            <Form.Group className="mb-4" controlId="finalizacion">
+                <Form.Control type="date" placeholder="Fecha en que finaliza la toma de medicamentos" name="finalizacion" value={fecha} onChange={ (ev) => setFecha(ev.target.value)}/>
+                <Form.Text className="text-muted">
+                Fecha en que finaliza la toma de medicamentos
+                </Form.Text>
+            </Form.Group>
+            <div className="d-flex justify-content-center">
+                <Button type="button" onClick={agregarMedicamento} className="btn btn-agregar">
+                    Agregar
+                </Button>
+            </div>
+            <p className="fw-bold text-center mt-4">Lista de medicamentos agregados:</p>
+            <ul className="lista-agregada-meds">
+                {medicamentos.map((medicamento, i) =>
+                <li key={i} className="shadow mb-3">
+                    <span className="fw-bold">{medicamento.nombre}</span><br/>
+                    <span className="me-5">
+                        <span className="fw-bold">Debe tomar el medicamento cada:</span> {medicamento.horas} hs
+                    </span>
+                    <span>
+                        <span className="fw-bold">Finaliza el:</span> {medicamento.fecha}
+                    </span>
+                </li>
+                )}
+            </ul>
         </div>
     )
 }
